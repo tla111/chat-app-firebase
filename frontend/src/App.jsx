8dcbc8f0c41ab3450d0214534a9c3539c57baeb0
Tim La <tlamarketing111@gmail.com>
2023-01-16 08:55:33 -0500

    cleaned up auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react'
 import './App.css'
+import { Auth } from './components/Auth'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>App</h1>
+      <Auth />
     </div>
   )
 }
